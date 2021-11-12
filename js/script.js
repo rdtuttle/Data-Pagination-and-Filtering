99da0efc738f72301609df8ae4228c9bc4b8dc93
Russ Tuttle <russelltuttle1@gmail.com>
2021-11-12 17:59:51 -0600

    added function
    function to add elements onto page

--- js/script.js
+++ js/script.js
@@ -4,20 +4,36 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+  var startIndex = (page * 9) -9;
+  var endIndex = page * 9;
+  var studentList = document.querySelector(".student-list");
+  studentList.innerHTML = "";
+  for (let i= 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      
+     var studentItem = `  
+      <li class="student-item cf">
+        <div class="student-details">
+          <img class="avatar" src=${data[i].picture.medium} alt="Profile Picture">
+          <h3>${data[i].name.first} ${data[i].name.last}</h3>
+          <span class="email">${data[i].email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">${data[i].registered.date}</span>
+        </div>
+    </li>
+    `
 
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+
+    }
+
+  }
+}
 
 
 /*
